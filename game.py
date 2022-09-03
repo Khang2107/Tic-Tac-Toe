58e9586745490009152818c96824305dc2a3710a
Khang2107 <nguvy056@mymail.unisa.edu.au>
2022-09-03 20:32:13 +0930

    Building my first game project-tic tac toe

---
+++ game.py
@@ -0,0 +1,48 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' 'for _ in range(9)] # we use a single list to rep 3x3 board
+        self.current_winner = None  # keep track of winner
+        
+    def print_board(self):
+        # this only gets the rows
+        for row in [self.board[ i*3: (i+1)*3] for i in range(3)]:   # [i*3 to (i+1)* 3]
+            print('| ' + ' | '.join(row) + ' |')
+    
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc (tell us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+            
+    def avaiable_move(self):    # return empty spots
+        # moves = []
+        # for (i, spot) in enumerate(self.board):    
+        # # ['x', 'x', 'o'] --> [(0, 'x'), (1, 'x'), (2, 'o')]
+        #     if spot == ' ':     # if spot is blank
+        #         moves.append(i)
+        # return moves
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
+        
+    
+    def empty_squares(self):    # check if there is any square
+        return ' ' in self.board    # return boolean
+    
+    def num_empty_squares(self):
+        # return len(self.available_moves())  # count how many empty spots
+        return self.board.count(' ')    # count number of spaces in the board
+    
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_boards_nums() # tell us which number corresponds to which box
+        
+    letter = 'X'    # set a starting number
+    # iterate while the game still has empty squares
+    # we do not worry about the winner because we will just return that
+    # which breaks the loop
+    while game.empty_squares:
+        # get the move from appropriate player
+        if letter == "0":
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
---
+++ player.py
@@ -0,0 +1,43 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+        
+    # we want all players to get their next move
+    def get_move(self, game):
+        pass
+    
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):     # initialise superclass
+        super().__init__(letter)    # call initialier in the superclass Player
+        
+    def get_move(self, game):
+        # get a random valid spot that is empty for our next move
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):     # initialise superclass
+        super().__init__(letter)    # call initialier in the superclass Player
+        
+    def get_move(self, game):
+        valid_square = False    
+        val = None  # user has not input value yet
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we are going to check that this is a correct value by trying to cast
+            # it to an integer, and if it is not, then we say it is invalid
+            try:
+                val = int(square)   
+                # raise error when they do not pass in int
+                # raise if spot input not available
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True     # if it is successful
+            except ValueError:
+                print("Invalid square. Try again")
+                
+        return val
